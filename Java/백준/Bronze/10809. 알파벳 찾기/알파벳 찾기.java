461b7e6497c66c4af4d02786809317e0da731652
sitsoserdiado <sitsoserdiado@gmail.com>
2025-07-30 18:06:41 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 132 ms, Memory: 15888 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (arr[c - 'a'] == -1) {
+                arr[c - 'a'] = i;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + " ");
+        }
+        
+        bw.close();
+    }
+}
