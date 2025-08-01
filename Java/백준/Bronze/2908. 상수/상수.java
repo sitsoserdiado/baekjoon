8814da270e602b791a569f60f0e132adb71a819a
sitsoserdiado <sitsoserdiado@gmail.com>
2025-08-01 18:31:43 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- Java/백준/Bronze/2908. 상수/상수.java
+++ Java/백준/Bronze/2908. 상수/상수.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -10,18 +11,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] ss = br.readLine().split(" ");
-        String[] sss = ss[0].split("");
-        String[] ssss = ss[1].split("");
-        int a = (Integer.parseInt(sss[2]) * 100) + (Integer.parseInt(sss[1]) * 10) + (Integer.parseInt(sss[0]));
-        int b = (Integer.parseInt(ssss[2]) * 100) + (Integer.parseInt(ssss[1]) * 10) + (Integer.parseInt(ssss[0]));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        if (a > b) {
-            bw.write(String.valueOf(a));
-        } else {
-            bw.write(String.valueOf(b));
-        }
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
 
+        bw.write(String.valueOf(a > b ? a : b));
         bw.close();
+
+        // String[] ss = br.readLine().split(" ");
+        // String[] sss = ss[0].split("");
+        // String[] ssss = ss[1].split("");
+        // int a = (Integer.parseInt(sss[2]) * 100) + (Integer.parseInt(sss[1]) * 10) + (Integer.parseInt(sss[0]));
+        // int b = (Integer.parseInt(ssss[2]) * 100) + (Integer.parseInt(ssss[1]) * 10) + (Integer.parseInt(ssss[0]));
+
+        // if (a > b) {
+        //     bw.write(String.valueOf(a));
+        // } else {
+        //     bw.write(String.valueOf(b));
+        // }
+
+        // bw.close();
     }
 }
