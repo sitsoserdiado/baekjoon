cc91304755b26744a4fb45d1ae211b3ae6b37a61
sitsoserdiado <sitsoserdiado@gmail.com>
2025-08-04 07:04:24 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        int result = 1;
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                result = 0;
+                break;
+            }
+        }
+
+        bw.write(String.valueOf(result));
+        bw.close();
+    }
+}
